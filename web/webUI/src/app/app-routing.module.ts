c3fea7e4f3aa10a9af33dccf7a4512e52d04dce2
mhsharifi96 <mh.sh7676@gmail.com>
2020-08-04 20:56:05 +0430

    add footer header search compoent

--- web/webUI/src/app/app-routing.module.ts
+++ web/webUI/src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+// component
+import { HomePageComponent } from './components/home/home-page/home-page.component';
+
+
+const routes: Routes = [
+  {path : "",component : HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/webUI/src/app/app.module.ts
+++ web/webUI/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+
+// component
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { FooterComponent } from './components/common/footer/footer.component';
 import { HomePageComponent } from './components/home/home-page/home-page.component';
+import { SearchComponent } from './components/home/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomePageComponent
+    HomePageComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
